263d469bb2fd76cb1e4e87d9536915ccc6bcb15c
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-10-14 21:35:31 -0300

    chore: aula 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+function ListaInscritos({ alunos, children }) {
+  return (
+    <ul>
+      {alunos && alunos.map((aluno) => <li key={aluno}>{aluno}</li>)}
+      {(!alunos || alunos.length === 0) && (
+        <div>{children}</div>
+      )}
+    </ul>
+  );
+}
 
 function App() {
+  const [alunos, setAlunos] = useState([]);
+  const [novo, setNovo] = useState("");
+
+  const processarOnClick = () => {
+    setAlunos([...alunos, novo]);
+    setNovo('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ListaInscritos alunos={alunos}>
+        Aguardando o primeiro aluno a ser cadastrado
+      </ListaInscritos>
+      Novo aluno:{" "}
+      <input
+        type="text"
+        value={novo}
+        onChange={(event) => setNovo(event.target.value)}
+      />
+      <button onClick={processarOnClick}>Adicionar Aluno</button>
     </div>
   );
 }
